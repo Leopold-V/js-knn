b6596ed4e2b90c95535527f08fa6491bf75bdb38
Leopold-V <leopoldV79@gmail.com>
2021-02-27 09:50:50 +0100

    prettier

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const getdata = async (jsonfilename) => {
 const DistanceFromTargetArray = async (target, jsonfilename) => {
   const arr = await getdata(jsonfilename);
   const copyBeforeModif = JSON.parse(JSON.stringify(arr));
-  const long = Math.sqrt(
-    target.reduce((a,b) => a+(b*b), 0)
-  );
+  const long = Math.sqrt(target.reduce((a, b) => a + b * b, 0));
 
   const newArr = arr.map((ele, i) => {
-    delete arr[i][Object.keys(arr[i])[Object.keys(arr[i]).length-1]];
-    return Math.sqrt(Object.values(ele).reduce((a, b) => a + (b * b), 0));
+    delete arr[i][Object.keys(arr[i])[Object.keys(arr[i]).length - 1]];
+    return Math.sqrt(Object.values(ele).reduce((a, b) => a + b * b, 0));
   });
   const tab = newArr.map((ele, i) => {
-    return { value: ele, species: Object.values(copyBeforeModif[i])[Object.values(copyBeforeModif[i]).length-1] };
+    return {
+      value: ele,
+      species: Object.values(copyBeforeModif[i])[Object.values(copyBeforeModif[i]).length - 1],
+    };
   });
   const final = tab.map((ele) => {
     return { position: Math.abs(ele.value - long), species: ele.species };
@@ -30,10 +31,12 @@ const FindSpecies = async (target, jsonfilename) => {
   const count = {};
 
   for (let i = 0; i < 8; i++) {
-    if (count[arr[i][Object.keys(arr[i])[Object.keys(arr[i]).length-1]]] !== undefined) {
-      count[arr[i][Object.keys(arr[i])[Object.keys(arr[i]).length-1]]]++;
+    if (
+      count[arr[i][Object.keys(arr[i])[Object.keys(arr[i]).length - 1]]] !== undefined
+    ) {
+      count[arr[i][Object.keys(arr[i])[Object.keys(arr[i]).length - 1]]]++;
     } else {
-      count[arr[i][Object.keys(arr[i])[Object.keys(arr[i]).length-1]]] = 1;
+      count[arr[i][Object.keys(arr[i])[Object.keys(arr[i]).length - 1]]] = 1;
     }
   }
   console.log(count);
@@ -42,9 +45,9 @@ const FindSpecies = async (target, jsonfilename) => {
 
 //Example with iris demo dataset
 
-const jsonfile = 'iris.json';
-const target = [5.1, 3.5, 1.4, 0.2]
-const target2 = [7.7,3,6.1,2.3];
+const jsonfile = "iris.json";
+const target = [5.1, 3.5, 1.4, 0.2];
+const target2 = [7.7, 3, 6.1, 2.3];
 const target3 = [5, 2.3, 3.3, 1];
 FindSpecies(target, jsonfile);
 FindSpecies(target2, jsonfile);
